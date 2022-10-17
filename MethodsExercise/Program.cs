76a2b794aa6fa863d9c3a28b4b08654cfb94091a
Yvette Lerma <lerma.yvette@hotmail.com>
2022-10-16 23:00:32 -0600

    alittle story

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            var name = "Yvette";
+            var favoriteColor = "Yellow";
+            var favoriteAnimal = "Ant Eater";
+            var favoriteBand = "Maverick City";
+
+            Console.WriteLine($"What is your name? {name}. And my favorite color is {favoriteColor}. I went to the zoo and saw my favorite animal once, which is an {favoriteAnimal}. And for my birthday my husband took me to see my favorite band {favoriteBand}.");
         }
     }
 }
