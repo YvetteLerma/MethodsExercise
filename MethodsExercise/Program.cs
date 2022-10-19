c0382a2dec11b9d31818993a2d836e72b2dbb89f
Yvette Lerma <lerma.yvette@hotmail.com>
2022-10-19 11:08:10 -0600

    added code

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,16 +1,56 @@
-﻿namespace MethodsExercise
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            
-            var name = "Yvette";
-            var favoriteColor = "Yellow";
-            var favoriteAnimal = "Ant Eater";
-            var favoriteBand = "Maverick City";
+﻿Console.WriteLine($"What is your name?");
+var name = Console.ReadLine();
 
-            Console.WriteLine($"What is your name? {name}. And my favorite color is {favoriteColor}. I went to the zoo and saw my favorite animal once, which is an {favoriteAnimal}. And for my birthday my husband took me to see my favorite band {favoriteBand}.");
-        }
-    }
+Console.WriteLine($"What's your favorite color?");
+var color = Console.ReadLine();
+
+Console.WriteLine($"What's your favorite animal?");
+var animal = Console.ReadLine();
+
+Console.WriteLine($"What is your favorite band?");
+var favoriteBand = Console.ReadLine();
+
+{
+    Console.WriteLine($"My name is {name}. My favorite color is {color}. I have a favorite animal, yup you guessed it, it is an {animal}. Does {favoriteBand} count as a favorite band?");
 }
+
+static int Add(int x, int y)
+{
+    int sum = x + y;
+    return sum;
+}
+int y = Add(8, 7);
+Console.WriteLine($"{y}");
+
+static int Subtract(int b, int a)
+
+{
+    int diff = b - a;
+    return diff;
+}
+int a = Subtract(22, 8);
+Console.WriteLine($"{a}");
+
+static int Multiply(int x, int y)
+{
+    int mult = x * y;
+return mult;
+}
+int c = Multiply(11, 2);
+Console.WriteLine("y");
+
+static int Divide(int x, int y)
+{
+    int div = x * y;
+    return div;
+}
+int d = Divide(11, 2);
+Console.WriteLine("y");
+
+static int Modulus(int x, int y)
+{
+    int modul = x * y;
+    return modul;
+}
+int e = Modulus(11, 2);
+Console.WriteLine("y");
